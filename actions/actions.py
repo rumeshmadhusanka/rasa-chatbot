f0a5940f9d61a71bdfb48c47c0543fa0facce7d6
rumesh <rumesh.m.s16@gmail.com>
2021-10-30 11:13:14 +0530

    added custom actions

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,35 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionMostPopularSong(Action):
+
+    def name(self) -> Text:
+        return "action_get_most_popular_song"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="Action: this is the most popular song")
+
+        return []
+
+
+class ActionMostPopularSongOfArtist(Action):
+
+    def name(self) -> Text:
+        return "action_get_most_popular_song_of_artist"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        artist = next(tracker.get_latest_entity_values("artist"), None)
+        print(artist)
+        dispatcher.utter_message(text="Action: this is the most popular song of artist ")
+
+        return []
