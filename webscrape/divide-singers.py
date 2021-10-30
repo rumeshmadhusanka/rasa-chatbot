97351e48237de29b61112f792e105eebfc3c38e6
rumesh <rumesh.m.s16@gmail.com>
2021-10-30 21:33:58 +0530

    added doc id

--- webscrape/divide-singers.py
+++ webscrape/divide-singers.py
@@ -13,8 +13,11 @@ def predict_sentiment(singers):
             return "positive"
     return "negative"
 
+
+i = 1
 for d in data:
     ins = dict()
+    ins["id"] = i
     ins["title"] = d["title"]
     ins["body"] = d["body"].replace("\n\r\n", "\n").replace("\r\n", "\n").replace(".", " ")
     singers = d["singer"].split("සමග")
@@ -23,5 +26,6 @@ for d in data:
     ins["streams"] = randrange(5000, 50000, 100)
     ins["sentiment"] = predict_sentiment(singers)
     new_data.append(ins)
+    i += 1
 
 json.dump(new_data, out_file, ensure_ascii=False, indent=4)
