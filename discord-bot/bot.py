433bcdb43fd7340b76ab53fd40b18b3c181d5e45
rumesh <rumesh.m.s16@gmail.com>
2021-11-20 16:22:57 +0530

    added discord bot

---
+++ discord-bot/bot.py
@@ -0,0 +1,36 @@
+import os
+
+import aiohttp
+import discord
+from dotenv import load_dotenv
+
+from logger import get_logger
+
+load_dotenv()
+client = discord.Client()
+rasa_url = "http://localhost:5005/webhooks/rest/webhook"
+logger = get_logger("discord_bot")
+
+
+@client.event
+async def on_ready():
+    logger.info('Discord bot started as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    logger.debug(message.author.name + ": " + message.content)
+    if message.author == client.user:
+        return
+    content = {
+        "sender": message.author.name,
+        "message": message.content
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(rasa_url, json=content) as resp:
+            rasa_reply = await resp.json()
+            rasa_reply = rasa_reply[0]['text']
+            await message.channel.send(rasa_reply)
+
+
+client.run(os.environ['DISCORD_TOKEN'])
