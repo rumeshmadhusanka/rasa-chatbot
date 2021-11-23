7657ffe4a498e55aded819cd774eb926f7280bc1
rumesh <rumesh.m.s16@gmail.com>
2021-11-23 21:29:38 +0530

    helper and actions divided [skip ci]

--- actions/actions.py
+++ actions/actions.py
@@ -1,103 +1,12 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-import json
-import random
 from typing import Any, Text, Dict, List
 
-from nltk.metrics.distance import jaccard_distance
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-songs_file = "../webscrape/si-songs-final.json"
-songs = json.load(open(songs_file))
+from actions.helper import extract_artist, get_logger, \
+    suggest_song
 
-
-def build_index(instances):
-    title_index = dict()
-    body_index = dict()
-    for ins in instances:
-        doc_id = ins["id"]
-        title_words = ins["title"].split()
-        for pos in range(len(title_words)):
-            if title_words[pos] in title_index:
-                title_index[title_words[pos]].append((doc_id, pos))
-            else:
-                title_index[title_words[pos]] = [(doc_id, pos)]
-
-        body_words = ins["body"].split()
-        for pos in range(len(body_words)):
-            if body_words[pos] in body_index:
-                body_index[body_words[pos]].append((doc_id, pos))
-            else:
-                body_index[body_words[pos]] = [(doc_id, pos)]
-
-    vocabulary = set(title_index.keys()).union(body_index.keys())
-
-    return title_index, body_index, vocabulary
-
-
-def song_of_artist(artist):
-    lst = []
-    for ins in songs:
-        for singer in ins["singers"]:
-            if jaccard_distance(singer, artist) < 0.3:
-                lst.append(ins["title"])
-    return lst
-
-
-def most_popular_song():
-    popular_song = ""
-    max_views = 0
-    for ins in songs:
-        for singer in ins["singers"]:
-            if max_views < ins["streams"]:
-                max_views = ins["streams"]
-                popular_song = ins["title"]
-    return popular_song, max_views
-
-
-def most_popular_of_artist(artist):
-    popular_song = ""
-    max_views = 0
-    for ins in songs:
-        for singer in ins["singers"]:
-            if jaccard_distance(singer, artist) < 0.3:
-                if max_views < ins["streams"]:
-                    max_views = ins["streams"]
-                    popular_song = ins["title"]
-    return popular_song, max_views
-
-
-def suggest_song(mood):
-    lst = []
-    for ins in songs:
-        sent = ins["sentiment"]
-        if sent == mood:
-            lst.append(ins["title"])
-    return random.choice(lst)
-
-
-def match_lyrics(guess):
-    pass
-    # todo implement proximity query
-
-
-def extract_artist(message):
-    if "ගේ" in message:
-        return message.split("ගේ")[0]
-    if "ගෙ" in message:
-        return message.split("ගෙ")[0]
-    if "ගායනා" in message:
-        return message.split("ගායනා")[0]
-    if "කියපු" in message:
-        return message.split("කියපු")[0]
+logger = get_logger("actions")
 
 
 class ActionMostPopularSong(Action):
@@ -108,6 +17,7 @@ class ActionMostPopularSong(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        logger.debug("ActionMostPopularSong")
         dispatcher.utter_message(text="Action: this is the most popular song")
 
         return []
@@ -121,12 +31,11 @@ class ActionMostPopularSongOfArtist(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        artist = next(tracker.get_latest_entity_values("artist"), None)
-        print(artist)
-
-        artist = extract_artist(tracker.latest_message)
+        artist_obj = tracker.latest_message['entities'][0]
+        artist = artist_obj['value']
+        logger.debug("ActionMostPopularSongOfArtist")
+        logger.debug(str(artist_obj))
         dispatcher.utter_message(text="Action: this is the most popular song of artist " + artist)
-
         return []
 
 
@@ -139,19 +48,20 @@ class ActionSongsOfSinger(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         artist = extract_artist(tracker.latest_message)
+        logger.debug("ActionSongsOfSinger")
         dispatcher.utter_message(text="Action: this is the songs of this singer " + artist)
 
         return []
 
 
 class ActionMatchLyrics(Action):
-
     def name(self) -> Text:
         return "action_match_lyrics"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        logger.debug("ActionMatchLyrics")
         dispatcher.utter_message(text="Action: match song lyrics")
 
         return []
@@ -164,7 +74,9 @@ class ActionSuggestHappySong(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text="Action: suggest song on happy mood")
+        string = suggest_song("positive")
+        logger.debug("ActionSuggestHappySong")
+        dispatcher.utter_message(text=string)
 
         return []
 
@@ -176,11 +88,8 @@ class ActionSuggestSadSong(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text="Action: suggest song on sad mood")
+        string = suggest_song("negative")
+        logger.debug("ActionSuggestSadSong")
+        dispatcher.utter_message(text=string)
 
         return []
-
-
-if __name__ == '__main__':
-    _, _, index = build_index(songs)
-    print(index)
---
+++ actions/helper.py
@@ -0,0 +1,166 @@
+import json
+import random
+import logging
+from nltk.metrics.distance import jaccard_distance
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
+
+
+songs_file = "webscrape/si-songs-final.json"
+songs = json.load(open(songs_file))
+jaccard_dis = 0.3
+logger = get_logger("actions_helper")
+
+
+def build_index(instances):
+    title_index = dict()
+    body_index = dict()
+    for ins in instances:
+        doc_id = ins["id"]
+        title_words = ins["title"].split()
+        for pos in range(len(title_words)):
+            if title_words[pos] in title_index:
+                title_index[title_words[pos]].append((doc_id, pos))
+            else:
+                title_index[title_words[pos]] = [(doc_id, pos)]
+
+        body_words = ins["body"].split()
+        for pos in range(len(body_words)):
+            if body_words[pos] in body_index:
+                body_index[body_words[pos]].append((doc_id, pos))
+            else:
+                body_index[body_words[pos]] = [(doc_id, pos)]
+
+    vocabulary = set(title_index.keys()).union(body_index.keys())
+
+    return title_index, body_index, vocabulary
+
+
+def songs_of_artist(artist):
+    lst = []
+    artist = set(artist)
+    for ins in songs:
+        for singer in ins["singers"]:
+            singer = set(singer)
+            if jaccard_distance(singer, artist) < 0.3:
+                lst.append(ins["title"])
+    return lst
+
+
+def most_popular_song():
+    popular_song = ""
+    max_views = 0
+    for ins in songs:
+        for singer in ins["singers"]:
+            if max_views < ins["streams"]:
+                max_views = ins["streams"]
+                popular_song = ins["title"]
+    return popular_song, max_views
+
+
+def most_popular_of_artist(artist):
+    popular_song = ""
+    max_views = 0
+    artist = set(artist)
+    for ins in songs:
+        for singer in ins["singers"]:
+            singer = set(singer)
+            if jaccard_distance(singer, artist) < 0.3:
+                if max_views < ins["streams"]:
+                    max_views = ins["streams"]
+                    popular_song = ins["title"]
+    return popular_song, max_views
+
+
+def suggest_song(mood):
+    lst = []
+    for ins in songs:
+        sent = ins["sentiment"]
+        if sent == mood:
+            lst.append(ins)
+    selected = random.choice(lst)
+    title, body, singers = selected['title'], selected['body'], selected['singers']
+    if len(singers) > 1:
+        sing = singers[0] + " සහ " + singers[1]
+    else:
+        sing = singers[0]
+    string = sing + " ගායනා කරන " + title + " ගීතය අහන්න\n " + "** 🎵🎵🎵\n" + body + "🎵🎵🎵 **"
+    return string
+
+
+def extract_artist(message):
+    if "ගේ" in message:
+        return message.split("ගේ")[0]
+    if "ගෙ" in message:
+        return message.split("ගෙ")[0]
+    if "ගායනා" in message:
+        return message.split("ගායනා")[0]
+    if "කියපු" in message:
+        return message.split("කියපු")[0]
+
+
+def get_song_by_id(id_song):
+    for ins in songs:
+        if ins['id'] == id_song:
+            return ins['title'], ins['body'], ins['singers']
+
+
+def match_lyrics(guess):
+    guess = guess.strip().split()
+    assert len(guess) > 0
+    body_keys = list(body_index.keys())
+    prospects = dict()
+    for word in guess:
+        prospects[word] = []
+        for key in body_keys:
+            if jaccard_distance(set(word), set(key)) < jaccard_dis:
+                prospects[word].extend(body_index[key])
+    # print(prospects)
+    final_prospects = []
+    for i in range(len(guess) - 1):
+        cur_word = guess[i]
+        nex_word = guess[i + 1]
+        cur_data = prospects[cur_word]
+        nex_data = prospects[nex_word]
+        for pair in cur_data:
+            s_id = pair[0]
+            pos = pair[1]
+            for nx_pair in nex_data:
+                sn_id = nx_pair[0]
+                pos_n = nx_pair[1]
+                if s_id == sn_id:
+                    if abs(pos_n - pos) <= 4:
+                        final_prospects.append(s_id)
+    if len(final_prospects) > 0:
+        song_id = max(set(final_prospects), key=final_prospects.count)
+        logger.debug("Final prospects: " + str(final_prospects) + " " + str(song_id))
+        chosen_song = get_song_by_id(song_id)
+        logger.debug("Chosen song: " + str(chosen_song))
+        return chosen_song
+    return None
+
+
+if __name__ == '__main__':
+    mps = most_popular_song()
+    print(mps)
+    mpsa = most_popular_of_artist("ෆන්කි ඩර්ට්")
+    print(mpsa)
+    ss = suggest_song("positive")
+    print(ss)
+    soa = songs_of_artist("ෆන්කි ඩර්ට්")
+    print(soa)
+
+    title_index, body_index, vocabulary = build_index(songs)
+    # print(body_index)
+    match = match_lyrics("රැස් විහිදෙන දුර අවකාසේ")
+    print(match)
+    # print(match)
